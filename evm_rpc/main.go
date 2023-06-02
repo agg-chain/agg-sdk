01cb0f045ac765428f87851a8b4f20e5d9695f96
Myron <Myron.zhang@coinstore.com>
2023-06-03 01:24:32 +0800

    feat: show gas fee

--- evm_rpc/main.go
+++ evm_rpc/main.go
@@ -12,7 +12,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
 
 var (
@@ -26,7 +25,7 @@ func main() {
 			return
 		}
 
-		if strings.Contains(sdkconfig.RpcUrl, "127.0.0.1") {
+		if os.Getenv("ENV") == "dev" {
 			w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
 			w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
 			w.Header().Set("content-type", "application/json")             //返回数据格式是json
