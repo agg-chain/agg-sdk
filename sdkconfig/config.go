1051e721b1c381bef6d2b2e50614edf604c9bc6d
Myron <Myron.zhang@coinstore.com>
2023-06-02 22:00:40 +0800

    feat: add gasfee

--- sdkconfig/config.go
+++ sdkconfig/config.go
@@ -3,7 +3,7 @@ package sdkconfig
 import "math/big"
 
 var (
-	//RpcUrl = "http://127.0.0.1:26657"
-	RpcUrl  = "https://agg-rpc.web3-idea.xyz"
+	RpcUrl = "http://127.0.0.1:26657"
+	//RpcUrl  = "https://agg-rpc.web3-idea.xyz"
 	ChainId = big.NewInt(11000)
 )
